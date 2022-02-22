fd1232b1ce03eabffb3f4b62c41e205910d739f3
bsplvmxm <i.maksim_01@mail.ru>
2022-02-22 17:30:24 +0300

    task number 1 devedu

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA = Console.ReadLine();
+            int A = Convert.ToInt32(numberA);
+            string numberB = Console.ReadLine();
+            int B = Convert.ToInt32(numberB);
+
+            int result1 = A / B;
+            Console.WriteLine($"Answer 1 = {result1}");
+            int result2 = A % B;
+            Console.WriteLine($"Answer 2 = {result2}");
+        }
+    }
+}
