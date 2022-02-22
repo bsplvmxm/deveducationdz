f26ea9685d5268de71f30274d21ad70a1b4d11d6
bsplvmxm <i.maksim_01@mail.ru>
2022-02-22 17:34:03 +0300

    task number 3 devedu

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string A:");
+            string A = Console.ReadLine();
+            Console.WriteLine("Enter string B:");
+            string B = Console.ReadLine();
+            string copy = A;
+
+            A = B;
+            B = copy;
+           
+
+            Console.WriteLine($"String A: {A} / String B: {B}");
+
+        }
+    }
+}
