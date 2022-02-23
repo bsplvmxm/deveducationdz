7dc8357e384930b5870fd74444ce5f70891a713f
bsplvmxm <i.maksim_01@mail.ru>
2022-02-23 15:39:09 +0300

    task number 1 devedu

---
+++ HW2/Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A:");
+            string numberA = Console.ReadLine();
+            int A = Convert.ToInt32(numberA);
+            Console.WriteLine("Enter B:");
+            string numberB = Console.ReadLine();
+            int B = Convert.ToInt32(numberB);
+
+            if (A>B)
+            {
+                int result = A + B;
+                Console.WriteLine($"*A>B* Answer: {result}");
+            }
+            else if (A==B)
+            {
+                int result = A * B;
+                Console.WriteLine($"*A==B* Answer: {result}");
+            }
+            else if (A<B)
+            {
+                int result = A - B;
+                Console.WriteLine($"*A<B* Answer: {result}");
+            }
+
+        }
+    }
+}
