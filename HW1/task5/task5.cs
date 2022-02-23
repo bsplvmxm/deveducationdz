a575312f9f6fd8c9d412cf4ae59f553a6e5c0235
bsplvmxm <i.maksim_01@mail.ru>
2022-02-23 15:49:08 +0300

    task number 5 devedu

---
+++ HW1/task5/task5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter coord X1, X2, Y1, Y2:");
+
+            string coordX1 = Console.ReadLine();
+            int X1 = Convert.ToInt32(coordX1);
+            string coordX2 = Console.ReadLine();
+            int X2 = Convert.ToInt32(coordX2);
+            string coordY1 = Console.ReadLine();
+            int Y1 = Convert.ToInt32(coordY1);
+            string coordY2 = Console.ReadLine();
+            int Y2 = Convert.ToInt32(coordY2);
+
+            Console.WriteLine($"Coords: ({X1};{Y1}),({X2};{Y2})");
+
+            int A = (Y2 - Y1) / (X2 - X1);
+            int B = Y2 - (A * X2);
+            string sign;
+
+            if (B >= 0)
+            {
+                sign = "+";
+            }
+            else
+            {
+                sign = "";
+            }
+
+            Console.WriteLine($"Answer: Y={A}X{sign}{B}");
+        }
+    }
+}
