a4bef91e9dd254e6ba96ed1f38d496523d9ac2ee
bsplvmxm <i.maksim_01@mail.ru>
2022-02-25 17:07:36 +0300

    task number 1 devedu

---
+++ HW3/Task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            int number = 1;
+            for (int i=0; i<B; i++)
+            {
+                number *= A;
+            }
+
+            Console.WriteLine($"Answer: {A} ^ {B} = {number}");
+        }
+    }
+}
