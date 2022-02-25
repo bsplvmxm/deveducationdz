2bf4a4b15b0378a21e1f90964314512150b7b9ce
bsplvmxm <i.maksim_01@mail.ru>
2022-02-25 18:16:36 +0300

    task number 5 devedu

---
+++ HW3/Task 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            int summ = 0;
+
+            if (A < B)
+            {
+                for (int i = A; i < B; i++)
+                {
+                    if (i % 7 == 0)
+                    {
+                        summ += i;
+                        i++;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = B; i < A; i++)
+                {
+                    if (i % 7 == 0)
+                    {
+                        summ += i;
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine($"Answer: {summ}");
+        }
+    }
+}
