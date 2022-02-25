63ea1aea187f9d993a7cda8a295f53bce32a0d10
bsplvmxm <i.maksim_01@mail.ru>
2022-02-25 20:26:39 +0300

    task number 9 devedu

---
+++ HW3/Task 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int decade;
+            int amount = 0;
+
+            while (number != 0)
+            {
+                decade = number % 10;
+                if (number % 2 != 0)
+                {
+                    amount++;
+                }
+                number /= 10;
+            }
+            Console.WriteLine($"Answer: {amount}");
+        }
+    }
+}
