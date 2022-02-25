28178e513b9aa010e0809e1bdae837900a40e64e
bsplvmxm <i.maksim_01@mail.ru>
2022-02-25 17:23:04 +0300

    task number 2 devedu

---
+++ HW3/Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+
+            int A = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i=A; i<=1000; i++)
+            {
+                if (i % A == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
