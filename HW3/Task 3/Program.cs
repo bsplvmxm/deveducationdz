fe4b95340d446746b5f1a1c7aec6609e8f943dc3
bsplvmxm <i.maksim_01@mail.ru>
2022-02-25 17:36:23 +0300

    task number 3 devedu

---
+++ HW3/Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int amount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int sqr = i * i;
+                if (i > 0 && i % 2 == 0 && sqr < A)
+                {
+                    amount++;
+                }
+            }
+            Console.WriteLine($"Answer: {amount}");
+        }
+    }
+}
