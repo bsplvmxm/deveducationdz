09b6366f460dcb9fd68350e1abd132834b9b86bb
bsplvmxm <i.maksim_01@mail.ru>
2022-02-25 17:55:14 +0300

    task number 4 devedu

---
+++ HW3/Task 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int maxDivider = 0;
+
+            for (int i=1; i<n; i++)
+            {
+                if (A % i == 0 && i!=A)
+                {
+                    if (i > maxDivider)
+                    {
+                        maxDivider = i;
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine($"Answer: {maxDivider}");
+        }
+    }
+}
