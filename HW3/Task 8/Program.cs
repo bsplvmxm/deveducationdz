c6c9644b677e45e84fe454a6b3763a0b9f05cb30
bsplvmxm <i.maksim_01@mail.ru>
2022-02-27 16:11:21 +0300

    task number 8 devedu

---
+++ HW3/Task 8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            int b = N;
+            int half = 0;
+
+            while (Math.Pow(half, 3) != N)
+            {
+                half = (a + b) / 2;
+                if (Math.Pow(half,3) < N)
+                {
+                    a = half;
+                }
+                else
+                {
+                    b = half;
+                }
+            }
+            Console.WriteLine($"Answer: {half}");
+        }
+    }
+}
