f2612e3f6e01c8b5d7dbb541dedd79d2f4dcab81
bsplvmxm <i.maksim_01@mail.ru>
2022-02-27 15:47:14 +0300

    task number 6 devedu

---
+++ HW3/Task 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter positive N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int first = 1;
+            int second = 1;
+            int fibonacci = 0;
+            int i = 2;
+
+                while (i < N)
+                {
+                    fibonacci = first + second;
+                    first = second;
+                    second = fibonacci;
+                    i++;
+                }
+                Console.WriteLine($"Answer: {fibonacci}");
+        }
+    }
+}
