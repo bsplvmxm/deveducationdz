d8a50db5c03e93a9cc08b61d7a7fe85256e77d9f
bsplvmxm <i.maksim_01@mail.ru>
2022-02-27 20:23:04 +0300

    task number 11 devedu

---
+++ HW3/Task 11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int decade;
+
+            for (int i = 1; i < N; i++)
+            {
+                int tmp = i;
+                int summPositive = 0;
+                int summNegative = 0;
+                while (tmp != 0)
+                {
+                    decade = tmp % 10;
+                    if (tmp % 2 == 0)
+                    {
+                        summPositive += decade;
+                    }
+                    else
+                    {
+                        summNegative += decade;
+                    }
+                    tmp /= 10;
+                }
+                if (summPositive > summNegative)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+
+        }
+    }
+}
