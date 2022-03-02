35d2e8eb1588f32a2211d180a30671095c6ed7cb
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-02 18:16:30 +0300

    task number 5, method GetNumberInWords

--- HW4/HW4/IfElse.cs
+++ HW4/HW4/IfElse.cs
@@ -51,5 +51,132 @@ namespace HW4
 
             return result;
         }
+
+        public static string GetNumberInWords(int number)
+        {
+            int tenth = number / 10;
+            int unit = number % 10;
+            string tenthNum = "";
+            string unitNum = "";
+            string result = "";
+
+            if (number > 19)
+            {
+                if (tenth == 2)
+                {
+                    tenthNum = "Двадцать";
+                }
+                else if (tenth == 3)
+                {
+                    tenthNum = "Тридцать";
+                }
+                else if (tenth == 4)
+                {
+                    tenthNum = "Сорок";
+                }
+                else if (tenth == 5)
+                {
+                    tenthNum = "Пятьдесят";
+                }
+                else if (tenth == 6)
+                {
+                    tenthNum = "Шестьдесят";
+                }
+                else if (tenth == 7)
+                {
+                    tenthNum = "Семьдесят";
+                }
+                else if (tenth == 8)
+                {
+                    tenthNum = "Восемьдесят";
+                }
+                else
+                {
+                    tenthNum = "Девяносто";
+                }
+                if (unit == 1)
+                {
+                    unitNum = "Один";
+                }
+                else if (unit == 2)
+                {
+                    unitNum = "Два";
+                }
+                else if (unit == 3)
+                {
+                    unitNum = "Три";
+                }
+                else if (unit == 4)
+                {
+                    unitNum = "Четыре";
+                }
+                else if (unit == 5)
+                {
+                    unitNum = "Пять";
+                }
+                else if (unit == 6)
+                {
+                    unitNum = "Шесть";
+                }
+                else if (unit == 7)
+                {
+                    unitNum = "Семь";
+                }
+                else if (unit == 8)
+                {
+                    unitNum = "Восемь";
+                }
+                else
+                {
+                    unitNum = "Девять";
+                }
+                result = tenthNum +" "+ unitNum;
+            }
+            else
+            {
+                if (number == 10)
+                {
+                    result = "Десять";
+                }
+                else if (number == 11)
+                {
+                    result = "Одиннадцать";
+                }
+                else if (number == 12)
+                {
+                    result = "Двенадцать";
+                }
+                else if (number == 13)
+                {
+                    result = "Тринадцать";
+                }
+                else if (number == 14)
+                {
+                    result = "Четырнадцать";
+                }
+                else if (number == 15)
+                {
+                    result = "Пятнадцать";
+                }
+                else if (number == 16)
+                {
+                    result = "Шестнадцать";
+                }
+                else if (number == 17)
+                {
+                    result = "Семнадцать";
+                }
+                else if (number == 18)
+                {
+                    result = "Восемнадцать";
+                }
+                else if (number == 19)
+                {
+                    result = "Девятнадцать";
+                }
+
+            }
+                return result;
+        }
     }
 }
