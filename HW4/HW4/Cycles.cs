b600a35064b633ac7cbde5e00e63eeabd9086d34
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-02 23:09:57 +0300

    task number 12, method FindIfThereSameDigits

--- HW4/HW4/Cycles.cs
+++ HW4/HW4/Cycles.cs
@@ -168,6 +168,26 @@ namespace HW4
 
             return tmp;
         }
+
+        public static bool FindIfThereSameDigits(int number1, int number2)
+        {
+            bool check = false;
+            int decade;
+
+            while (number1 != 0)
+            {
+                decade = number1 % 10;
+                for (int i = number2; i != 0; i /= 10)
+                {
+                    if (i % 10 == decade)
+                    {
+                        check = true;
+                    }
+                }
+                number1 /= 10;
+            }
+            return check;
+        }
     }
 
 }
