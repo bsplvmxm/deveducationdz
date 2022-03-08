9068db3628138b8c5ac12251d0a6a4b83875a735
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-08 22:41:33 +0300

    add mthd GenerateArray, mthd WriteArray

--- HW4/HW4/Array.cs
+++ HW4/HW4/Array.cs
@@ -8,5 +8,48 @@ namespace HW4
 {
     class Array
     {
+        public static int[] GenerateArray(int lenght)
+        {
+            Random r = new Random();
+
+            int[] arr = new int[lenght];
+
+            for (int i=0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(-100,100);
+            }
+            return arr;
+        }
+
+        public static void WriteArray(int[] array)
+        {
+            for (int i=0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        public static void GetSolutionOfMinimumElement()
+        {
+            Console.Write("Enter lenght of array: ");
+            int[] arr = GenerateArray(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Your array: ");
+            WriteArray(arr);
+            Console.Write($"Min element: {FindMinimumElementOfArray(arr)}");
+             
+        }
+        public static int FindMinimumElementOfArray(int[] a)
+        {
+            int min = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            return min;
+        }
     }
 }
