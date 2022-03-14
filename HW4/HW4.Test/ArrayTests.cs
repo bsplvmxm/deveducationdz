1feda87652ecc949805a2a69e321bbff107fdc94
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-15 01:06:42 +0300

    +test FindMinimumElementOfArray, FindMaximumElementOfArray

---
+++ HW4/HW4.Test/ArrayTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace HW4.Test
+{
+    public class ArrayTests
+    {
+        [TestCase(new int[] { 6, 1, 63, 22 }, 1)]
+        [TestCase(new int[] { -9, 5, -29 }, -29)]
+        [TestCase(new int[] { -11, 99, 4 }, -11)]
+        public void FindMinimumElementOfArrayTest(int[] a, int expected)
+        {
+            int actual = MyArray.FindMinimumElementOfArray(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 6, 1, 63, 22 }, 63)]
+        [TestCase(new int[] { -9, -5, -29 }, -5)]
+        [TestCase(new int[] { -11, 99, 4 }, 99)]
+        public void FindMaximumElementOfArrayTest(int[] a, int expected)
+        {
+            int actual = MyArray.FindMaximumElementOfArray(a);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
