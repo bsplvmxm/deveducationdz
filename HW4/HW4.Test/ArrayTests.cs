9a147e991841296e833beafcd01fcc98627b611a
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-15 01:19:17 +0300

    +test FindSummNumbersOddIndex, ReverseArray

--- HW4/HW4.Test/ArrayTests.cs
+++ HW4/HW4.Test/ArrayTests.cs
@@ -40,5 +40,23 @@ namespace HW4.Test
             int actual = MyArray.FindIndexMaxElementOfArray(a);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCase(new int[] { 6, 1, 63, 22 }, 23)]
+        [TestCase(new int[] { -9, -5, -29 }, -5)]
+        [TestCase(new int[] { -11, 69, 4, -2, 89 }, 67)]
+        public void FindSummNumbersOddIndexTest(int[] a, int expected)
+        {
+            int actual = MyArray.FindSummNumbersOddIndex(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 6, 1, 63, 22 }, new int[] { 22, 63, 1, 6 })]
+        [TestCase(new int[] { -9, -5, -29 }, new int[] { -29, -5, -9 })]
+        [TestCase(new int[] { -11, 69, 4, -2, 89 }, new int[] { 89, -2, 4, 69, -11 })]
+        public void ReverseArrayTest(int[] a, int[] expected)
+        {
+            int[] actual = MyArray.ReverseArray(a);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
