193f45abe4b53a145d06569485682dcf47129dd9
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-14 17:37:03 +0300

    +test mthd GetSolutionOfFormulaTest without exception

--- HW4/HW4.Test/VariablesTests.cs
+++ HW4/HW4.Test/VariablesTests.cs
@@ -3,8 +3,15 @@ using System;
 
 namespace HW4.Test
 {
-    class VariablesTests
+    public class VariablesTests
     {
-
+        [TestCase(1,2,9)]
+        [TestCase(4,6,28)]
+        [TestCase(-3,-6,-7)]
+        public void GetSolutionOfFormulaTest(int A, int B, int expected)
+        {
+            int actual = Variables.GetSolutionOfFormula(A, B);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
