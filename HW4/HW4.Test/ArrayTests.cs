5f5a19253d47613ad1029fb2037940855fc4263f
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-15 01:11:48 +0300

    +test FindIndexMinElementOfArray, FindIndexMaxElementOfArray

--- HW4/HW4.Test/ArrayTests.cs
+++ HW4/HW4.Test/ArrayTests.cs
@@ -22,5 +22,23 @@ namespace HW4.Test
             int actual = MyArray.FindMaximumElementOfArray(a);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCase(new int[] { 6, 1, 63, 22 }, 1)]
+        [TestCase(new int[] { -9, 5, -29 }, 2)]
+        [TestCase(new int[] { -11, 99, 4 }, 0)]
+        public void FindIndexMinElementOfArrayTest(int[] a, int expected)
+        {
+            int actual = MyArray.FindIndexMinElementOfArray(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 6, 1, 63, 22 }, 2)]
+        [TestCase(new int[] { -9, -5, -29 }, 1)]
+        [TestCase(new int[] { -11, 69, 4, -2, 89 }, 4)]
+        public void FindIndexMaxElementOfArrayTest(int[] a, int expected)
+        {
+            int actual = MyArray.FindIndexMaxElementOfArray(a);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
