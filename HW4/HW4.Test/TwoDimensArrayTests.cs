1f8bb19bbd5c65cd80e165e6945bc41df7f203fb
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-15 23:02:04 +0300

    +test FindIndexMaxElementOfTDArray, FindIndexMinElementOfTDArray +except

--- HW4/HW4.Test/TwoDimensArrayTests.cs
+++ HW4/HW4.Test/TwoDimensArrayTests.cs
@@ -38,5 +38,39 @@ namespace HW4.Test
             int[,] arr = TDMock.GetMock(type);
             Assert.Throws<Exception>(() => TwoDimensArray.FindMaxElementOfTDArray(arr));
         }
+
+        [TestCase(TDAMockType.first, "(2,1)")]
+        [TestCase(TDAMockType.second, "(2,1)")]
+        [TestCase(TDAMockType.third, "(0,2)")]
+        public void FindIndexMinElementOfTDArrayTest(TDAMockType type, string expected)
+        {
+            int[,] arr = TDMock.GetMock(type);
+            string actual = TwoDimensArray.FindIndexMinElementOfTDArray(arr);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(TDAMockType.empty)]
+        public void FindIndexMinElementOfTDArrayTest_WhenLenghtsLess1_ShouldThrowException(TDAMockType type)
+        {
+            int[,] arr = TDMock.GetMock(type);
+            Assert.Throws<Exception>(() => TwoDimensArray.FindIndexMinElementOfTDArray(arr));
+        }
+
+        [TestCase(TDAMockType.first, "(2,2)")]
+        [TestCase(TDAMockType.second, "(1,2)")]
+        [TestCase(TDAMockType.third, "(2,2)")]
+        public void FindIndexMaxElementOfTDArray(TDAMockType type, string expected)
+        {
+            int[,] arr = TDMock.GetMock(type);
+            string actual = TwoDimensArray.FindIndexMaxElementOfTDArray(arr);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(TDAMockType.empty)]
+        public void FindIndexMaxElementOfTDArrayTest_WhenLenghtsLess1_ShouldThrowException(TDAMockType type)
+        {
+            int[,] arr = TDMock.GetMock(type);
+            Assert.Throws<Exception>(() => TwoDimensArray.FindIndexMaxElementOfTDArray(arr));
+        }
     }
 }
